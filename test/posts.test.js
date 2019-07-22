1b1ad559083a3a8227d093fdb62bde8de5ee1fdc
Zach Butler <zach.standley93@gmail.com>
2019-07-22 16:08:45 -0700

    we created our first Post route and have a passing test

---
+++ test/posts.test.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('post routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  const agent = request.agent(app);
+  beforeEach(async() => {
+    const user = await User.create({
+      username: 'lalall',
+        password: 'wkejrnwkejrn',
+        profilePhotoUrl: 'hebrjwhebwjebr'
+    });
+    return agent 
+      .post('/api/v1/auth/signin')
+      .send({
+        username: 'lalall',
+        password: 'wkejrnwkejrn',
+      })
+  })
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a post', () => {
+    return agent
+      .post('/api/v1/posts/')
+      .send({ photoUrl: 'somePhoto', caption: 'someCaption', tags: ['cool', 'dog', 'blessed'] })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          user: expect.any(String),
+          photoUrl: 'somePhoto',
+          caption: 'someCaption',
+          tags: ['cool', 'dog', 'blessed'],
+          __v: 0
+        });
+      });
+  });
+});
