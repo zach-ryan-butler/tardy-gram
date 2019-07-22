0c275c7c7ddbbe990bb31262128a6cef0de4b42d
Zach Butler <zach.standley93@gmail.com>
2019-07-22 14:26:59 -0700

    we created our signin post route for auth with a passing test

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -25,6 +26,30 @@ describe('app routes', () => {
         username: 'lalall',
         password: 'wkejrnwkejrn',
         profilePhotoUrl: 'hebrjwhebwjebr'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'lalall',
+          passwordHash: expect.any(String),
+          profilePhotoUrl: 'hebrjwhebwjebr',
+          __v: 0
+        })
+      })
+  });
+
+  it('can sign in an existing user and provide token', async() => {
+    const user = await User.create({
+      username: 'lalall',
+        password: 'wkejrnwkejrn',
+        profilePhotoUrl: 'hebrjwhebwjebr'
+    });
+
+    return request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        username: 'lalall',
+        password: 'wkejrnwkejrn',
       })
       .then(res => {
         expect(res.body).toEqual({
